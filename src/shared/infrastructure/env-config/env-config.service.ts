4207a31e156a20bc5a942caba4537ba3539f843b
Rafael Scholant <rafael.scholant@fortbrasil.com.br>
2023-09-20 08:22:56 -0300

    feat: implementing env config interface

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { EnvConfig } from '@shared/infrastructure/env-config/env-config.interface';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+  constructor(private readonly configService: ConfigService) {}
+  getAppPort(): number {
+    return this.configService.get<number>('PORT');
+  }
+  getNodeEnv(): string {
+    return this.configService.get<string>('NODE_ENV');
+  }
+}

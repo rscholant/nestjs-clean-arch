1eedbc1ce734ebb7872930800560c1dceeb6632f
Rafael Scholant <rafael.scholant@fortbrasil.com.br>
2023-10-16 15:42:25 -0300

    feat: add getters in user entity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -9,4 +9,16 @@ export class UserEntity {
   constructor(public readonly props: UserProps) {
     this.props.createdAt = this.props.createdAt ?? new Date();
   }
+  get name(): string {
+    return this.props.name;
+  }
+  get email(): string {
+    return this.props.email;
+  }
+  get password(): string {
+    return this.props.password;
+  }
+  get createdAt(): string {
+    return this.props.name;
+  }
 }
